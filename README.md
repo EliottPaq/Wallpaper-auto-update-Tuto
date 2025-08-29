655f39411b237c2e777fa9cf116e1d74b4e34431
eliott <eliopaq@gmail.com>
2025-08-29 16:40:18 +0200

    add resources

--- README.md
+++
@@ -1,2 +0,0 @@
-# Wallpaper-auto-update-Tuto
-This is a tutorial on how to set up an auto wallpaper update depending on anything you want (date, time etc)
---
+++ SetWallpaper.ps1
@@ -0,0 +1,27 @@
+# SetWallpaper.ps1
+
+function Set-Wallpaper($path) {
+    Add-Type @"
+    using System.Runtime.InteropServices;
+    public class Wallpaper {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    }
+"@
+    [Wallpaper]::SystemParametersInfo(20, 0, $path, 3)
+}
+
+$hour = (Get-Date).Hour #get the hour
+$basePath = "C:\Your\Path\Wallpaper-auto-update-Tuto\images" #Edit this path
+
+if ($hour -ge 5 -and $hour -lt 10) { #this choose a images depending of the time of the day
+    $wallpaper = "$basePath\morning.jpg"
+} elseif ($hour -ge 10 -and $hour -lt 17) {
+    $wallpaper = "$basePath\afternoon.jpg"
+} elseif ($hour -ge 18 -and $hour -lt 22) {
+    $wallpaper = "$basePath\evening.jpg"
+} else {
+    $wallpaper = "$basePath\night.jpg"
+}
+
+Set-Wallpaper $wallpaper
